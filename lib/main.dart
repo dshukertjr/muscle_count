2bc4a6b8c074710289342f24871ed47aa187331f
dshukertjr <dshukertjr@gmail.com>
2019-03-02 01:55:20 +0900

    created list with dropdown list

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+  MyHomePage();
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -27,6 +28,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   FirebaseUser user;
   TextEditingController workoutNameController = TextEditingController();
+  int workoutCount;
+  bool submitting;
 
   @override
   void initState() {
@@ -38,17 +41,14 @@ class _MyHomePageState extends State<MyHomePage> {
     user = await FirebaseAuth.instance.currentUser();
     if (user == null) {
       user = await FirebaseAuth.instance.signInAnonymously();
-      setState(() {});
     }
+    setState(() {});
     print("user ${user.uid}");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Muscle Counter"),
-      ),
       body: SafeArea(
         child: user == null
             ? Center(
@@ -60,8 +60,8 @@ class _MyHomePageState extends State<MyHomePage> {
                     .where("uid", isEqualTo: user.uid)
                     .orderBy("createdAt", descending: true)
                     .snapshots(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snap) {
+                builder:
+                    (BuildContext context, AsyncSnapshot<QuerySnapshot> snap) {
                   if (snap.connectionState == ConnectionState.waiting)
                     return Center(
                       child: CircularProgressIndicator(),
@@ -73,7 +73,17 @@ class _MyHomePageState extends State<MyHomePage> {
                           itemCount: snap.data.documents.length,
                           itemBuilder: (BuildContext context, int index) {
                             final workout = snap.data.documents[index].data;
-                            return Text("${workout["name"]} ${workout["createdAt"]}");
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(workout["name"]),
+                                Text(
+                                  workout["createdAt"] == null?"":DateFormat.yMd().format(
+                                    workout["createdAt"],
+                                  ),
+                                ),
+                              ],
+                            );
                           },
                         ),
                       ),
@@ -82,19 +92,49 @@ class _MyHomePageState extends State<MyHomePage> {
                           Expanded(
                             child: TextFormField(
                               controller: workoutNameController,
+                              decoration: InputDecoration(
+                                prefix: submitting != true
+                                    ? Container()
+                                    : CircularProgressIndicator(),
+                              ),
                             ),
+                          ),
+                          DropdownButton<int>(
+                            value: workoutCount,
+                            onChanged: (int val) {
+                              setState(() {
+                                workoutCount = val;
+                              });
+                            },
+                            items: List.generate(12, (i) => i + 1)
+                                .map<DropdownMenuItem<int>>((int count) {
+                              return DropdownMenuItem<int>(
+                                value: count,
+                                child: Text("$count"),
+                              );
+                            }).toList(),
                           ),
                           IconButton(
                             icon: Icon(Icons.add),
-                            onPressed: () {
-                              final String workoutName =
-                                  workoutNameController.text;
-                              Firestore.instance.collection("workouts").add({
-                                "name": workoutName,
-                                "uid": user.uid,
-                                "createdAt": FieldValue.serverTimestamp(),
-                              });
-                            },
+                            onPressed: workoutNameController.text.isNotEmpty
+                                ? () async {
+                                    final String workoutName =
+                                        workoutNameController.text;
+                                    submitting = true;
+                                    setState(() {});
+                                    await Firestore.instance
+                                        .collection("workouts")
+                                        .add({
+                                      "name": workoutName,
+                                      "uid": user.uid,
+                                      "createdAt": FieldValue.serverTimestamp(),
+                                    });
+                                    submitting = false;
+                                    workoutCount = null;
+                                    workoutNameController.clear();
+                                    setState(() {});
+                                  }
+                                : null,
                           ),
                         ],
                       ),
